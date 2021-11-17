43766e7ab95ac7a33080c926a408f5c7a320662e
whatthefoobar <irina.mntn@gmail.com>
2021-11-17 13:09:02 +0100

    camera follows sprite

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(600, 400, Phaser.AUTO);
+var game = new Phaser.Game(1000, 700, Phaser.AUTO);
 
 game.state.add("state0", demo.state0);
 game.state.add("state1", demo.state1);
--- state0.js
+++ state0.js
@@ -1,7 +1,15 @@
 var demo = {};// we only need to make a game obj once
+var centerX = 1500/2;
+var centerY = 1000/2;
+var adam;
+var speed = 6;
+
 demo.state0 = function(){};
 demo.state0.prototype = {
-    preload: function(){},
+    preload: function(){
+        game.load.image("adam", "assets/sprites/adam.png", 240, 370);
+        game.load.image("tree", "assets/backgrounds/treeBG.png");
+    },
     create: function()
     {
         game.stage.backgroundColor = "#c27c3e";
@@ -10,8 +18,32 @@ demo.state0.prototype = {
         // game.input.keyboard.addKey(Phaser.Keyboard.ONE).onDown.add(changeState, null, null, 1);
        
         addChangeStateEventListeners();
+        game.world.setBounds(0, 0, 2813, 1000);
+
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_All;// adjust screen size
+
+        var treeBG = game.add.sprite(0, 0, "tree");
+
+        adam = game.add.sprite(centerX, centerY, "adam");
+        // adam.anchor.x = 0.5; //is drawn from the center of the img
+        // adam.anchor.y = 0.5; or
+        adam.anchor.setTo(0.5, 0.5);
+        adam.scale.setTo(0.6, 0.6);
+
+        game.camera.follow(adam);// but we need to set bounds too
     },
-    update: function(){}
+    update: function(){
+        if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+            adam.x += speed;
+        }else if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+            adam.x -= speed;
+        }
+        if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+            adam.y -= speed;
+        }else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+            adam.y += speed;
+        }
+    }
 };
 
 function changeState(i, stateNum){
